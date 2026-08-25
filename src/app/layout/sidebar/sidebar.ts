ccdb342f39dec40b92c2aa35a79c4bdf3a6c4158
Nicolás Marulanda P <nmarulo@outlook.com>
2026-08-25 21:39:59 +0200

    Modificado: Menú lateral
    Ahora que tenemos una implementación que establece una porra en sesión, se agregan enlaces a las diferentes páginas que necesitan del identificador de la porra. NOTA: Algunos enlaces solo se muestran al organizador.

--- src/app/layout/sidebar/sidebar.ts
+++ src/app/layout/sidebar/sidebar.ts
@@ -1,11 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, computed, inject } from '@angular/core';
 import { RouterLink, RouterLinkActive } from '@angular/router';
+
+import { SelectedPoolSession } from '@app/services/session/selected-pool-session.service';
+import { OrganizerSession } from '@app/services/session/organizer-session.service';
 
 @Component({
   selector: 'app-sidebar',
   imports: [RouterLink, RouterLinkActive],
   host: { class: 'app-sidebar bg-body-secondary shadow', 'data-bs-theme': 'dark' },
   templateUrl: './sidebar.html',
-  styleUrl: './sidebar.css'
+  styleUrl: './sidebar.css',
 })
-export class Sidebar {}
+export class Sidebar {
+  private readonly organizer = inject(OrganizerSession);
+
+  protected readonly pool = inject(SelectedPoolSession).current;
+
+  protected readonly isOrganizer = computed(() => this.organizer.get(this.pool()?.publicId!) !== null);
+}
